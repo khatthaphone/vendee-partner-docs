331c07aa3fba650264b5cb726230706eb6a4a022
Khatthaphone Tamongsoumuangxay <8247636+khatthaphone@users.noreply.github.com>
2023-11-15 17:09:01 +0700

    Initial commit

--- app.config.ts
+++ app.config.ts
@@ -1,17 +1,24 @@
 export default defineAppConfig({
   docus: {
-    title: 'Docus',
-    description: 'The best place to start your documentation.',
+    title: 'Vendee X Partner Documentations',
+    description: 'Vendee X Partner Documentation',
     image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
+
     socials: {
-      twitter: 'nuxt_js',
-      github: 'nuxt-themes/docus',
+      twitter: '',
+      github: '',
+
       nuxt: {
         label: 'Nuxt',
         icon: 'simple-icons:nuxtdotjs',
         href: 'https://nuxt.com'
-      }
+      },
+
+      facebook: 'vendee',
+      tiktok: 'vendee_x',
+      instagram: ''
     },
+
     github: {
       dir: '.starters/default/content',
       branch: 'main',
@@ -19,20 +26,26 @@ export default defineAppConfig({
       owner: 'nuxt-themes',
       edit: true
     },
+
     aside: {
       level: 0,
       collapsed: false,
       exclude: []
     },
+
     main: {
       padded: true,
       fluid: true
     },
+
     header: {
-      logo: true,
+      logo: false,
       showLinkIcon: true,
       exclude: [],
-      fluid: true
-    }
+      fluid: true,
+      title: 'Vendee X Partner Documentation'
+    },
+
+    titleTemplate: ''
   }
 })
